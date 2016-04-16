e36d979294ea799f68b1d62b4c84a45717af129f
Michael S. Liebman <michael.liebman+dev@gmail.com>
2016-04-16 15:37:39 -0400

    Bump version to 9.0c.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Odoo',
-      version='9.0',
+      version='9.0c',
       description='Odoo on OpenShift',
       author='Michael Liebman',
       author_email='michael.liebman+dev@gmail.com',
